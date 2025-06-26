f5ef1fe2ab1be886dfcacc0ea0e89b1d5a2383bd
Nathan Eaton <nate.eaton.jr@gmail.com>
2025-06-26 07:54:40 -0500

    Update vite.config.js to remove debug code

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,6 @@ export default defineConfig(({ mode }) => {
   // Load .env files from the parent directory (the project root)
   const env = loadEnv(mode, path.resolve(__dirname, '..'), 'VITE_');
 
-  // Add the debug log back in just for this one test run!
-  console.log('--- DEBUG: Final check of loaded envs ---');
-  console.log(env);
-  console.log('--- END DEBUG ---');
-
   return {
     // The define property is NOT needed when using the VITE_ prefix.
     // Vite handles it automatically.
